7ebe3102a4720f5c5a748e9285fd65357e1f0c67
Tendai Nyandoro <tendai1@hotmail.com>
2021-03-05 01:22:18 +0200

    add the address

--- src/contact.js
+++ src/contact.js
@@ -2,5 +2,12 @@ import { ContextReplacementPlugin } from "webpack";
 
 function createContact() {
   const contact = document.createElement("div");
+  contact.classList.add("contact");
+
+  const phoneNumber = document.createElement("p");
+  phoneNumber.textContent = " Phone: +27 246 123 0987";
+
+  const address = document.createElement("p");
+  address.textContent = "Address: 39 Troupant Ave 2191, Magelissig, johannesburg, Gauteng, South Africa";
   
 }
