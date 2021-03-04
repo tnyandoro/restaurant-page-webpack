e0a9a654791d18e233c5c8b2c537069ac4b9b855
Tendai Nyandoro <tendai1@hotmail.com>
2021-03-05 00:21:40 +0200

    Add the header of the app

---
+++ src/restaurant.js
@@ -0,0 +1,8 @@
+import loadLanding from './landing';
+import loadMenu from './menu';
+import loadContact from './contact'
+
+function createHeader() {
+  const header = document.createElement("header");
+  header.classList.add("header");
+}
