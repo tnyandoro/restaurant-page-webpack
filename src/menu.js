c10823ffb597b96be2a1532cecb2459cb813eb42
Tendai Nyandoro <tendai1@hotmail.com>
2021-03-04 23:35:26 +0200

    Add the menu list

--- src/menu.js
+++ src/menu.js
@@ -1,1 +1,66 @@
-export const wizard = 'Charity';
+function createMenu() {
+  const menu = document.createElement("div");
+  menu.classList.add("menu");
+
+  menu.appendChild(
+    createMenuItem(
+      "MEZE",
+      "Hummus with Spicy Lamb Mince With pine kernels and served with pita bread"
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      "DIP & EAT",
+      "Mini Trio of Dips Choice of 3 mini dips. Served with grissini bread."
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      "POTATO CHIPS",
+      "Family Size Potato Chips"
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      "VILLAGE GREEK ",
+      "Tomatoes, cucumber, red onion, olives and feta."
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      "PLATED CHICKEN SOUVLAKI",
+      "3 Skewers served with chips, pita bread, chopped tomato, red onion and your choice of dip."
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      "MEZE",
+      "Hummus with Spicy Lamb Mince With pine kernels and served with pita bread"
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      "MEZE",
+      "Hummus with Spicy Lamb Mince With pine kernels and served with pita bread"
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      "MEZE",
+      "Hummus with Spicy Lamb Mince With pine kernels and served with pita bread"
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      "MEZE",
+      "Hummus with Spicy Lamb Mince With pine kernels and served with pita bread"
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      "MEZE",
+      "Hummus with Spicy Lamb Mince With pine kernels and served with pita bread"
+    )
+  );
+  
+}
