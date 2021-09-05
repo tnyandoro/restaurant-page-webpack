95f698554dd7a47a380443f8bd9504817f6c753a
Tendai <tendai1@hotmail.com>
2021-09-05 22:12:42 +0200

    jjjj

--- src/menu.js
+++ src/menu.js
@@ -1,58 +1,58 @@
 const createMenu = () => {
-  const menu = document.createElement("div");
-  menu.classList.add("menu");
+  const menu = document.createElement('div');
+  menu.classList.add('menu');
 
   menu.appendChild(
     createMenuItem(
-      "Meze",
-      "Hummus with Spicy Lamb Mince With pine kernels and served with pita bread"
-    )
+      'Meze',
+      'Hummus with Spicy Lamb Mince With pine kernels and served with pita bread',
+    ),
   );
   menu.appendChild(
     createMenuItem(
-      "Dipneat",
-      "Mini Trio of Dips Choice of 3 mini dips. Served with grissini bread."
-    )
+      'Dipneat',
+      'Mini Trio of Dips Choice of 3 mini dips. Served with grissini bread.',
+    ),
   );
   menu.appendChild(
     createMenuItem(
-      "Potatochips",
-      "Family Size Potato Chips"
-    )
+      'Potatochips',
+      'Family Size Potato Chips',
+    ),
   );
   menu.appendChild(
     createMenuItem(
-      "Pavlova",
-      "Tomatoes, cucumber, red onion, olives and feta."
-    )
+      'Pavlova',
+      'Tomatoes, cucumber, red onion, olives and feta.',
+    ),
   );
   menu.appendChild(
     createMenuItem(
-      "Calamari",
-      "3 Skewers served with chips, pita bread, chopped tomato, red onion and your choice of dip."
-    )
-  );  
+      'Calamari',
+      '3 Skewers served with chips, pita bread, chopped tomato, red onion and your choice of dip.',
+    ),
+  );
   menu.appendChild(
     createMenuItem(
-      "Chicken_full",
-      "10 Piece chicken. Drumsticks and thighs coal-grilled with olive oil, oregano and fresh lemon. Served with chips"
-    )
+      'Chicken_full',
+      '10 Piece chicken. Drumsticks and thighs coal-grilled with olive oil, oregano and fresh lemon. Served with chips',
+    ),
   );
 
   return menu;
 };
 
 const createMenuItem = (name, description) => {
-  const menuItem = document.createElement("div");
-  menuItem.classList.add("menu-item");
+  const menuItem = document.createElement('div');
+  menuItem.classList.add('menu-item');
 
-  const foodName = document.createElement("h2");
+  const foodName = document.createElement('h2');
   foodName.textContent = name;
 
-  const foodDescription = document.createElement("p");
+  const foodDescription = document.createElement('p');
   foodDescription.textContent = description;
 
-  const foodImage = document.createElement("img");
+  const foodImage = document.createElement('img');
   foodImage.src = `images/menu/${name.toUpperCase()}.png`;
   foodImage.alt = `${name}`;
 
@@ -64,8 +64,8 @@ const createMenuItem = (name, description) => {
 };
 
 const loadMenu = () => {
-  const main = document.getElementById("main");
-  main.textContent = "";
+  const main = document.getElementById('main');
+  main.textContent = '';
   main.appendChild(createMenu());
 };
 
